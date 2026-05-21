04e20fcb267fb6cc101d9ab40283168ee9317b76
saosoriop <161177359+saosoriop@users.noreply.github.com>
2026-05-21 15:01:34 -0500

    ejercicio 3

---
+++ Principal.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Notas;
+
+public class Principal {
+    
+public static void main(String[] args) {
+    VentanaPrincipal miVentanaPrincipal; /* Define la ventana principal */
+    miVentanaPrincipal= new VentanaPrincipal(); /* Crea la ventana principal */
+    miVentanaPrincipal.setVisible(true); /* Establece la ventana como visible */
+    }
+}
