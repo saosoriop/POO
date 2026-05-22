4fc51dc093a992da17c005a8beb451649eb8a028
saosoriop <161177359+saosoriop@users.noreply.github.com>
2026-05-22 10:01:50 -0500

    actividad5

---
+++ AddFriend.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package addfriend;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.lang.NumberFormatException;
+ 
+class AddFriend {
+ 
+    public static void main(String[] args){
+        Formulario frm = new Formulario();
+        frm.setVisible(true);       
+    }
+}
